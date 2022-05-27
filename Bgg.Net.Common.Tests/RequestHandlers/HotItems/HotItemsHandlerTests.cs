2ba959c70038f261c4f46382bdd7945cc889a627
James Kolts <kolts.james@gmail.com>
2022-05-27 12:49:08 -0400

    Add testcoverage for HotItems

--- Bgg.Net.Common.Tests/RequestHandlers/HotItems/HotItemsHandlerTests.cs
+++ Bgg.Net.Common.Tests/RequestHandlers/HotItems/HotItemsHandlerTests.cs
@@ -1,6 +1,5 @@
 ﻿using Bgg.Net.Common.Infrastructure;
 using Bgg.Net.Common.Models;
-using Bgg.Net.Common.Models.Requests;
 using Bgg.Net.Common.RequestHandlers.HotItems;
 using Bgg.Net.Common.Tests.Infrastructure.Xml;
 using Bgg.Net.Common.Tests.TestFiles;
@@ -8,7 +7,6 @@ using Bgg.Net.Common.Types;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Serilog;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
@@ -16,14 +14,131 @@ using System.Threading.Tasks;
 namespace Bgg.Net.Common.Tests.RequestHandlers.HotItems
 {
     [TestClass]
-    internal class HotItemsHandlerTests : HandlerTestBase
+    public class HotItemsHandlerTests : HandlerTestBase
     {
         private IHotItemsHandler? _handler;
 
         [TestMethod]
         public async Task GetHotItemsByType_Success()
         {
+            //Arrange
+            MockHttpClientGet(XmlGenerator.GenerateResourceXml(EmbeddedResource.HotXml), HttpStatusCode.OK);
+            MockBggDeserializer(
+                new HotItemList 
+                { 
+                    Item = new List<HotItem> 
+                    {
+                        new HotItem
+                        {
+                            Id = 1,
+                        },
+                        new HotItem
+                        {
+                            Id = 2
+                        }
+                    } 
+                });
 
+            _handler = new HotItemHandler(_deserializerMock.Object, _loggerMock.Object, _httpClientMock.Object);
+
+            //Act
+            var result = await _handler.GetHotItemsByType(HotItemType.BoardGame);
+
+            //Assert
+            _httpClientMock.Verify(x => x.GetAsync("hot?type=boardgame"), Times.Once);
+            result.Should().NotBeNull();
+            result.Errors.Should().BeNullOrEmpty();
+            result.HttpResponseCode.Should().Be(HttpStatusCode.OK);
+            result.Item.Item.Count.Should().Be(2);
+            result.Item.Item[0].Id.Should().Be(1);
+            result.Item.Item[1].Id.Should().Be(2);
+        }
+
+        [TestMethod]
+        public async Task GetHotItemsExtensible_Success()
+        {
+            //Arrange
+            var extension = new Extension
+            {
+                Value = new Dictionary<string, List<string>>
+                {
+                    {"type", new List<string> { "boardgame" } }
+                }
+            };
+
+            MockHttpClientGet(XmlGenerator.GenerateResourceXml(EmbeddedResource.HotXml), HttpStatusCode.OK);
+            MockBggDeserializer(
+                new HotItemList
+                {
+                    Item = new List<HotItem>
+                    {
+                        new HotItem
+                        {
+                            Id = 1,
+                        },
+                        new HotItem
+                        {
+                            Id = 2
+                        }
+                    }
+                });
+
+            _handler = new HotItemHandler(_deserializerMock.Object, _loggerMock.Object, _httpClientMock.Object);
+
+            //Act
+            var result = await _handler.GetHotItemsExtensible(extension);
+
+            //Assert
+            _httpClientMock.Verify(x => x.GetAsync("hot?type=boardgame"), Times.Once);
+            result.Should().NotBeNull();
+            result.Errors.Should().BeNullOrEmpty();
+            result.HttpResponseCode.Should().Be(HttpStatusCode.OK);
+            result.Item.Item.Count.Should().Be(2);
+            result.Item.Item[0].Id.Should().Be(1);
+            result.Item.Item[1].Id.Should().Be(2);
+        }
+
+        [TestMethod]
+        public async Task GetHotItemsExtensible_BadParam()
+        {
+            //Arrange
+            var extension = new Extension
+            {
+                Value = new Dictionary<string, List<string>>
+                {
+                    {"type", new List<string> { "boardgame" } },
+                    {"badParam", new List<string> { "boardgame" } }
+                }
+            };
+
+            MockHttpClientGet(XmlGenerator.GenerateResourceXml(EmbeddedResource.HotXml), HttpStatusCode.OK);
+            MockBggDeserializer(
+                new HotItemList
+                {
+                    Item = new List<HotItem>
+                    {
+                        new HotItem
+                        {
+                            Id = 1,
+                        },
+                        new HotItem
+                        {
+                            Id = 2
+                        }
+                    }
+                });
+
+            _handler = new HotItemHandler(_deserializerMock.Object, _loggerMock.Object, _httpClientMock.Object);
+
+            //Act
+            var result = await _handler.GetHotItemsExtensible(extension);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.HttpResponseCode.Should().BeNull();
+            result.Errors.Count.Should().Be(1);
+            result.Errors[0].Should().Be("'badParam' parameter is not supported for GetHotExtensible.");
+            result.Item.Should().BeNull();
         }
     }
 }
