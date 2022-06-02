48e75cf3327482eabc1884b26c27a6dfa074484b
James Kolts <kolts.james@gmail.com>
2022-06-02 13:52:51 -0400

    Removing unused extension method

--- Bgg.Net.Common.Tests/Infrastructure/Extensions/StringExtensionsTests.cs
+++ Bgg.Net.Common.Tests/Infrastructure/Extensions/StringExtensionsTests.cs
@@ -1,5 +1,4 @@
 ﻿using Bgg.Net.Common.Infrastructure.Extensions;
-using Bgg.Net.Common.Types;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,30 +7,6 @@ namespace Bgg.Net.Common.Tests.Infrastructure.Extensions
     [TestClass]
     public class StringExtensionsTests
     {
-        [TestMethod]
-        public void ToVersionType_Boardgame()
-        {
-            "boardgameversion".ToVersionType().Should().Be(VersionType.BoardGame);
-        }
-
-        [TestMethod]
-        public void ToVersionType_VideoGame()
-        {
-            "videogame".ToVersionType().Should().Be(VersionType.VideoGame);
-        }
-
-        [TestMethod]
-        public void ToVersionType_VideoGameCharacter()
-        {
-            "videogamecharacter".ToVersionType().Should().Be(VersionType.VideoGameCharacter);
-        }
-
-        [TestMethod]
-        public void ToVersionType_Invalid()
-        {
-            "anything else".ToVersionType().Should().BeNull();
-        }
-
         [TestMethod]
         public void UpperFirstChar_Empty()
         {
--- Bgg.Net.Common/Infrastructure/Extensions/StringExtensions.cs
+++ Bgg.Net.Common/Infrastructure/Extensions/StringExtensions.cs
@@ -1,9 +1,7 @@
-﻿using Bgg.Net.Common.Types;
-
-namespace Bgg.Net.Common.Infrastructure.Extensions
+﻿namespace Bgg.Net.Common.Infrastructure.Extensions
 {
     /// <summary>
-    /// 
+    /// Contains extensions for strings.
     /// </summary>
     public static class StringExtensions
     {
@@ -18,22 +16,6 @@ namespace Bgg.Net.Common.Infrastructure.Extensions
                 return null;
 
             return char.ToUpper(str[0]) + str[1..];
-        }
-
-        /// <summary>
-        /// Parses a string to a version type.
-        /// </summary>
-        /// <param name="value">The value to parse.</param>
-        /// <returns>A version type, or null.</returns>
-        public static VersionType? ToVersionType(this string value)
-        {
-            return value.ToLower() switch
-            {
-                "boardgameversion" => VersionType.BoardGame,
-                "videogame" => VersionType.VideoGame,
-                "videogamecharacter" => VersionType.VideoGameCharacter,
-                _ => null,
-            };
         }
     }
 }
