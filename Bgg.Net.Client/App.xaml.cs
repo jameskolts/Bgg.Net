2c25fd0a02cdd36c06e504e41698ec1459d1710a
James Kolts <kolts.james@gmail.com>
2022-06-07 11:25:22 -0400

    Update CollectionPage to use FlexLayout

--- Bgg.Net.Client/App.xaml.cs
+++ Bgg.Net.Client/App.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Bgg.Net.Client.IOC;
-using Bgg.Net.Client.Pages;
 
 namespace Bgg.Net.Client;
 
 public partial class App : Application
 {
+	public static int ScreenWidth, ScreenHeight;
+	public static string DeviceOs;
+	public static DevicePlatform Platform;
+
 	public App()
 	{
 		InitializeComponent();
@@ -15,7 +18,19 @@ public partial class App : Application
     protected override void OnStart()
     {
         base.OnStart();
-
+		GetDeviceInfo();
 		BootStrapper.Start(); //TODO find a place to stop this.
+    }
+
+	private void GetDeviceInfo()
+    {
+		var width = DeviceDisplay.MainDisplayInfo.Width;
+		var height = DeviceDisplay.MainDisplayInfo.Height;
+		var density = DeviceDisplay.MainDisplayInfo.Density;
+
+		ScreenWidth = (int)(width / density);
+		ScreenHeight = (int)(height / density);
+		DeviceOs = DeviceInfo.VersionString;
+		Platform = DeviceInfo.Current.Platform;
     }
 }
--- Bgg.Net.Client/Pages/Collection.xaml.cs
+++ Bgg.Net.Client/Pages/Collection.xaml.cs
@@ -22,36 +22,11 @@ public partial class Collection : ContentPage
     {
         collectionViewModel = BootStrapper.Resolve<ICollectionViewModel>();
         BindingContext = collectionViewModel;
-        SetCollectionViewSpan();
     }
 
     public async void InitAsync()
     {
         await collectionViewModel.GetCollection("JusticiarIV");
-    }
-
-    private void SetCollectionViewSpan()
-    {
-        var density = DeviceDisplay.Current.MainDisplayInfo.Density;
-        var deviceWidth = DeviceDisplay.Current.MainDisplayInfo.Width;
-        var columMath = (int)(deviceWidth / density / 350);
-        int columns;
-
-        if (columMath > 1)
-        {
-            columns = columMath - 2;
-        }
-        else
-        {
-            columns = 1;
-        }
-
-        CollectionView.ItemsLayout = new GridItemsLayout(columns, ItemsLayoutOrientation.Vertical)
-        {
-            HorizontalItemSpacing = 10,
-            VerticalItemSpacing = 10
-        };
-
     }
 
     protected void FilterBtn_Clicked(object sender, EventArgs e)
