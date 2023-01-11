4cc5591c173c42e8d4eacd5609a1b76ae9a13421
James Kolts <kolts.james@gmail.com>
2023-01-10 21:44:31 -0500

    Add BootStrap and Popper.  Stub out initial Navbar

---
+++ Bgg.Net.Web/Infrastructure/IOC/ClassFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Bgg.Net.Web.Infrastructure.IOC
+{
+    public static class ClassFactory
+    {
+        public static WebAssemblyHostBuilder RegisterWebComponents(this WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            return builder;
+        }
+    }
+}
--- Bgg.Net.Web/Program.cs
+++ Bgg.Net.Web/Program.cs
@@ -1,4 +1,5 @@
 using Bgg.Net.Web;
+using Bgg.Net.Web.Infrastructure.IOC;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RegisterWebComponents();
 
 await builder.Build().RunAsync();
