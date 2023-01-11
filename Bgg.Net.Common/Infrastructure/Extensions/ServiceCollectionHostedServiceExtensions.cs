1e0b1c37a41d93ab9193a947f54b91bcd5afaa2a
James Kolts <kolts.james@gmail.com>
2023-01-10 22:00:29 -0500

    Add classfactory for initial transition away from autofac

---
+++ Bgg.Net.Common/Infrastructure/Extensions/ServiceCollectionHostedServiceExtensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bgg.Net.Common.Infrastructure.Extensions
+{
+    /// <summary>
+    /// See https://dzone.com/articles/registering-a-type-as-an-interface-and-as-self-wit.
+    /// </summary>
+    public static class ServiceCollectionHostedServiceExtensions
+    {
+        /// <summary>
+        /// Register the last registration as its own type.
+        /// </summary>
+        /// <returns>The original <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" />.</returns>
+        public static IServiceCollection AsSelf(this IServiceCollection services)
+        {
+            var lastRegistration = services.LastOrDefault();
+            if (lastRegistration != null)
+            {
+                var implementationType = GetImplementationType(lastRegistration);
+
+                // When the last registration service type was already registered
+                // as its implementation type, bail out.
+                if (lastRegistration.ServiceType == implementationType)
+                {
+                    return services;
+                }
+
+                if (lastRegistration.ImplementationInstance != null)
+                {
+                    // Register "self" registration as the same instance
+                    services.Add(new ServiceDescriptor(
+                    implementationType,
+                    lastRegistration.ImplementationInstance));
+                }
+                else
+                {
+                    // Remove last registration
+                    services.Remove(lastRegistration);
+
+                    // Register "self" registration first
+                    if (lastRegistration.ImplementationFactory != null)
+                    {
+                        // Factory-based
+                        services.Add(new ServiceDescriptor(
+                        lastRegistration.ImplementationType,
+                        lastRegistration.ImplementationFactory,
+                        lastRegistration.Lifetime));
+                    }
+                    else
+                    {
+                        // Type-based
+                        services.Add(new ServiceDescriptor(
+                        lastRegistration.ImplementationType,
+                        lastRegistration.ImplementationType,
+                        lastRegistration.Lifetime));
+                    }
+
+                    // Re-register last registration, proxying our specific registration
+                    services.Add(new ServiceDescriptor(
+                    lastRegistration.ServiceType,
+                    provider => provider.GetService(implementationType),
+                    lastRegistration.Lifetime));
+                }
+            }
+
+            return services;
+        }
+
+        private static Type GetImplementationType(ServiceDescriptor descriptor)
+        {
+            if (descriptor.ImplementationType != null)
+            {
+                return descriptor.ImplementationType;
+            }
+
+            if (descriptor.ImplementationInstance != null)
+            {
+                return descriptor.ImplementationInstance.GetType();
+            }
+
+            if (descriptor.ImplementationFactory != null)
+            {
+                return descriptor.ImplementationFactory.GetType().GenericTypeArguments[1];
+            }
+
+            return null;
+        }
+    }
+}
+
---
+++ Bgg.Net.Common/Infrastructure/IOC/ClassFactory.cs
@@ -0,0 +1,40 @@
+﻿using Bgg.Net.Common.Infrastructure.Extensions;
+using Bgg.Net.Common.Infrastructure.Http;
+using Bgg.Net.Common.Infrastructure.Xml;
+using Bgg.Net.Common.RequestHandlers.Collection;
+using Bgg.Net.Common.RequestHandlers.Families;
+using Bgg.Net.Common.RequestHandlers.Forums;
+using Bgg.Net.Common.RequestHandlers.Guilds;
+using Bgg.Net.Common.RequestHandlers.HotItems;
+using Bgg.Net.Common.RequestHandlers.Search;
+using Bgg.Net.Common.RequestHandlers.Things;
+using Bgg.Net.Common.RequestHandlers.Threads;
+using Bgg.Net.Common.RequestHandlers.Users;
+using Bgg.Net.Common.Validation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bgg.Net.Common.Infrastructure.IOC
+{
+    public static class ClassFactory
+    {
+        public static IServiceCollection RegisterBggCommon(this IServiceCollection services)
+        {
+            services.AddTransient<IHttpClient, BggClient>().AsSelf();
+            services.AddTransient<ICollectionClient, CollectionClient>().AsSelf();
+            services.AddTransient<IBggDeserializer, BggDeserializer>().AsSelf();
+            services.AddTransient<IQueryBuilder, QueryBuilder>().AsSelf();
+            services.AddTransient<IRequestValidatorFactory>().AsSelf();
+            services.AddTransient<IThingHandler, ThingHandler>().AsSelf();
+            services.AddTransient<IFamilyHandler, FamilyHandler>().AsSelf();
+            services.AddTransient<IForumHandler, ForumHandler>().AsSelf();
+            services.AddTransient<IThreadHandler, ThreadHandler>().AsSelf();
+            services.AddTransient<IUserHandler, UserHandler>().AsSelf();
+            services.AddTransient<IGuildHandler, GuildHandler>().AsSelf();
+            services.AddTransient<ICollectionHandler, CollectionHandler>().AsSelf();
+            services.AddTransient<IHotItemsHandler, HotItemHandler>().AsSelf();
+            services.AddTransient<ISearchHandler, SearchHandler>().AsSelf();
+
+            return services;
+        }
+    }
+}
