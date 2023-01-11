d0438893f5d9e1dd095b1b2d358a6ed37a450c57
James Kolts <kolts.james@gmail.com>
2023-01-10 23:39:31 -0500

    Stub collection card

---
+++ Bgg.Net.Web/Pages/CollectionPage.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Bgg.Net.Web.Pages
+{
+    public partial class CollectionPage
+    {
+        public Common.Models.Collection? Collection { get; set; }
+
+        private async Task GetCollection()
+        {
+            var result = await _collectionHandler.GetCollectionByUserName("JusticiarIV");
+
+            Collection = result.Item;
+        }
+    }
+}
