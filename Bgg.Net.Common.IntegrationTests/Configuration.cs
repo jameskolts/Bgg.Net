38c40a0273a7b477aa6ab57fa71e0d4109f9367a
James Kolts <kolts.james@gmail.com>
2023-08-20 00:04:50 -0400

    Add Login Integration Test

---
+++ Bgg.Net.Common.IntegrationTests/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Bgg.Net.Common.IntegrationTests
+{
+    public static class Configuration
+    {
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")                
+                 .AddEnvironmentVariables()
+                 .Build();
+
+            return config;
+        }
+    }
+}
---
+++ Bgg.Net.Common.IntegrationTests/RequestHandlers/Login/BggLoginHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Bgg.Net.Common.Infrastructure.Http;
+using Bgg.Net.Common.RequestHandlers.Login;
+using Microsoft.Extensions.Logging;
+
+namespace Bgg.Net.Common.IntegrationTests.RequestHandlers.Login
+{
+    [TestClass]
+    public class BggLoginHandlerTests
+    {
+        private BggLoginHandler? _handler;
+
+        [TestMethod]
+        public async Task BggLoginHandler_LogsIn()
+        {
+            //Arrange
+            var config = Configuration.InitConfiguration();
+            var username = config["testUser:credentials:username"];
+            var password = config["testUser:credentials:password"];
+
+            _handler = new BggLoginHandler(new BggClient(), Mock.Of<ILogger<BggLoginHandler>>());
+
+            //Act
+            var result = await _handler.Login(username, password);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.UserName.Should().Be($"bggusername={username}");
+            result.Password.Should().NotBeNullOrWhiteSpace();
+            result.SessionId.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
